da39bbde4ccaac13b1294065580ab6d32cae2b62
PAULO VICTOR CAETANO DE OLIVEIRA PR114067 <paulocivictor@gmail.com>
2021-12-01 12:03:54 -0300

    start of historicopage editing

--- src/pages/Historico/index.js
+++ src/pages/Historico/index.js
@@ -2,11 +2,34 @@ import Navbar from '../../components/Navbar';
 
 import React from "react";
 
+const carregarHistorico = () => {
+    const n_regs = localStorage.length
+    for(let n in localStorage){
+        return(
+            localStorage.getItem(n)
+        )
+    }
+}
+
+
+
 const Historico = () => {
+    
     return(
         <>
             <Navbar/>
-            <h1>Página de historico</h1>    
+            <h1>Página de historico</h1> 
+            <table>
+                <tr>
+                    <th>Data</th>
+                    <th>Tipo</th>
+                    <th>Valor</th>
+                    <th>Descrição</th>
+                    <td></td>
+                    <td></td>
+                </tr>
+                {/*carregarHistorico()*/}
+            </table>
         </>
     )
     
